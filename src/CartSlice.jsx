194212528f1be8637878cd52e296b820126ecdb7
William Nguyen <nguyennamquoc.mar@gmail.com>
2024-12-21 12:25:28 -0500

    define logics for addItem, removeItem,updateQuantity of CartSlice

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -1,19 +1,35 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 export const CartSlice = createSlice({
-  name: 'cart',
+  name: "cart",
   initialState: {
     items: [], // Initialize items as an empty array
   },
   reducers: {
     addItem: (state, action) => {
-    
+      //extract data
+      const { name, img, cost } = action.payload;
+      //check existence and return the item
+      const isExisted = state.items.items.find((item) => item.name == name);
+      if (isExisted) {
+        isExisted.quantity++;
+      } else {
+        state.items.push({ name, img, cost, quantity: 1 });
+      }
     },
     removeItem: (state, action) => {
+      //overwrite the items array
+      state.items = state.items.filter((item) => item.name != action.payload);
     },
     updateQuantity: (state, action) => {
-
-    
+      //extract data from action payload
+      const { name, quantity } = action.payload;
+      
+      //find the item to be updated
+      const updatedItem = state.items.find((item) => item.name == name);
+      if (updatedItem) {
+        updatedItem.quantity = quantity;
+      }
     },
   },
 });
